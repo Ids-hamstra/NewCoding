f41755ccf983468c44e1943a9220c3fd60e9b1f7
Ids-hamstra <idshamstra.github99@gmail.com>
2025-03-31 17:26:40 +0200

    updates gedaan

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # NewCoding
+
+We are back from a break of not programming!
+We are going to stick with it now!
--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 let a = 20;
 let b = 30; 
 let c = a + b;
+
+console.log(c);
+
+const btn = document.querySelector("button");
+btn.onclick = () => console.log("Hello");
