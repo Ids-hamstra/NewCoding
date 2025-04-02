9bd64bb65ba6000dcc1b2e8285f7bea05e676e68
Ids-hamstra <idshamstra.github99@gmail.com>
2025-04-02 23:20:43 +0200

    op de toegevoegde paginas links naar home pagina + basis layout voor main feature

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-let a = 20;
-let b = 30; 
-let c = a + b;
 
-console.log(c);
-
-const btn = document.querySelector("button");
-btn.onclick = () => console.log("Hello");
-
-
-const div = document.createElement("div");
