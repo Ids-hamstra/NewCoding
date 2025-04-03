7fe0cf26d2500eec9a228aa85d44b0c26de83736
Ids-hamstra <idshamstra.github99@gmail.com>
2025-04-03 11:01:34 +0200

    Eerste functies toegevoegd om dagen bij te houden

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-const PRICE = 12;
+const PRICE_SIGA = 12;
+let aantalDagen = 0;
+let geldTotaal = 0;
+
+
+function verhoogAantalDagen() {
+    aantalDagen++;
+    document.getElementById("countDagen").innerText = aantalDagen;
+}
+
+function verminderAantalDagen() {
+    aantalDagen--;
+    document.getElementById("countDagen").innerText = aantalDagen;
+}
+
+
+//Wanneer iemand + knopt drukt bij geld bespaard, tel de prijs van een pakje op IPV steeds 1 erbij.
+function verhoogPrijs() {
+    geldTotaal + PRICE_SIGA;
+    document.getElementById("countMoney").innerText = geldTotaal;
+}
